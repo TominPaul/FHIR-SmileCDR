a366932e230e587ed9bb948f26237d8b5c02c2a9
TOMIN PAUL <tominpaul123@gmail.com>
2022-04-26 18:02:22 -0400

    Basic Tasks

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,7 +1,13 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.Patient;
 
 public class SampleClient {
@@ -21,7 +27,19 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
+        List<BundleEntryComponent> bundleEntry = response.getEntry();
+        List<Patient> patientsList = new ArrayList<>();
 
+        bundleEntry.forEach(be -> patientsList.add((Patient) be.getResource()));
+
+        Comparator<Patient> patientNameComparator = ((p1, p2) -> p1.getName().get(0).getGiven().get(0).getValueAsString().compareTo(p2.getName().get(0).getGiven().get(0).getValueAsString()));
+        patientsList.sort(patientNameComparator);
+
+        //Basic Tasks : Print patients' first name, last name, DOB & order by first name
+        patientsList.forEach(patient -> {
+            System.out.println(patient.getName().get(0).getGiven().get(0).getValueAsString()
+                    + " | " + patient.getName().get(0).getFamily()
+                    + " | " + patient.getBirthDate());
+        });
     }
-
 }
